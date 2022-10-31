8bb9b8e1f873d115ba28e5ae737880be8d49c7ca
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-10-31 18:56:50 +0900

    [add]systemスペックのvisitの位置を修正しました。

--- spec/system/tops_spec.rb
+++ spec/system/tops_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Tops", type: :system do
-  before do
-    visit "/"
-  end
   describe "check Products system" do
-  it "click About" do
+    it "Aboutリンクを押下後のAboutページへの遷移をテスト" do
+      visit "/"
       click_link "About"
       expect(current_path).to eq about_path
-  end
+    end
   end
 end
