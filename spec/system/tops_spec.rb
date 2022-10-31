6c8e31cc1ede6b05082e3e73ad838eea39c15eaf
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-10-31 19:24:05 +0900

    [add]Homeボタンを押下後の遷移のテストを追加

--- spec/system/tops_spec.rb
+++ spec/system/tops_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Tops", type: :system do
   describe "check Products system" do
-    it "Aboutリンクを押下後のAboutページへの遷移をテスト" do
+    it "トップページのAboutリンクを押下後のA遷移をテスト" do
       visit "/"
       click_link "About"
       expect(current_path).to eq about_path
+    end
+
+    it "アバウトページのHomeリンクを押下後の遷移をテスト" do
+      visit about_path
+      click_link "Home"
+      expect(current_path).to eq "/"
     end
   end
 end
