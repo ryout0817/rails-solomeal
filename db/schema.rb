8df3e6286c073290ca259b3036337a63d078d0d9
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-01 07:58:35 +0900

    [add]マイグレーションファイルにrollbackでnameカラムとintroductionカラムを追加しました。

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2022_10_31_135501) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "introduction"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
