9d07363d778e6ca7f02e4f4176223439d4735c11
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-02 23:23:20 +0900

    [add]サイドバーのリンクのテストをまとめました。

--- spec/system/tops_spec.rb
+++ spec/system/tops_spec.rb
@@ -2,19 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Tops", type: :system do
   describe "check Products system" do
-    it "トップページのサイドバーのリンクを押下後のA遷移をテスト" do
+    it "サイドバーのリンクの遷移をテスト" do
       visit "/"
       click_link "About"
       expect(current_path).to eq about_path
-
-      click_link  "SignUp"
-      expect(current_path).to eq new_user_registration_path
-    end
-
-    it "アバウトページのHomeリンクを押下後の遷移をテスト" do
-      visit about_path
       click_link "Home"
       expect(current_path).to eq "/"
+      click_link "SignUp"
+      expect(current_path).to eq new_user_registration_path
     end
   end
 end
