359bde1cbd931ffb7a0df4413a8d32575d842884
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-04 23:59:59 +0900

    [add]アカウント登録後編集画面に移行するように設定しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  private
+  def configure_sign_up_params
+    edit_user_registration_path(@user.id)
+  end
+  def after_sign_up_path_for(resource)
+    edit_user_registration_path(@user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   get '/', to: 'tops#index'
   get 'about', to: 'tops#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
