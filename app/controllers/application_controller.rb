ebb1b593f4663cd254ac0e095e208c8863a9cf0e
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-05 10:54:28 +0900

    [add]空行を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   def configure_sign_up_params
     edit_user_registration_path(@user.id)
   end
+
   def after_sign_up_path_for(resource)
     edit_user_registration_path(@user.id)
   end
