6ae558a029e06423fe35875b4d857c8c42cfe038
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-05 12:03:45 +0900

    [add]新規登録と更新時にnameカラムを取得できるように設定しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     edit_user_registration_path
   end
+
+  def configure_permitted_parameters
+    # 新規登録時にnicknameの取得を許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # 情報更新時にnicknameの取得を許可
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
