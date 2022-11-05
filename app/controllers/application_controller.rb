d0942618031725d8e52796312bd241edc6ee003e
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-05 11:04:08 +0900

    [add]ログイン後ユーザー編集画面に移動するように遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(resource)
     edit_user_registration_path(@user.id)
   end
+
+  def after_sign_in_path_for(resource)
+    edit_user_registration_path
+  end
 end
