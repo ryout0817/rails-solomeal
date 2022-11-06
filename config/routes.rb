71855518b6ebe013b931085c674f1f23a22afd58
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-06 22:59:47 +0900

    [add]プロフィールページへのリンクとルートを修正しました。

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   }
   get '/', to: 'tops#index'
   get 'about', to: 'tops#about'
-  resources :accoounts, :only => [:show]
+  resources :accounts, :only => [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
