22452d150bdb6922eb02ee62940ea847f04bdd5e
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-07 16:49:27 +0900

    [add]プロフィール情報を更新するためのコントローラーとルーティングを追加しました。

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,5 +4,15 @@ class AccountsController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = current_user
+    if @user.update(params.require(:user).permit(:name, :introduction))
+      redirect_to account_path(@user)
+    else
+      render "edit"    
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   }
   get '/', to: 'tops#index'
   get 'about', to: 'tops#about'
-  resources :accounts, :only => [:show, :edit]
+  resources :accounts, :only => [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
