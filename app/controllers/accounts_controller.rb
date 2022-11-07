1395028771e147197a6ab8e4e49487351d026713
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-07 14:41:25 +0900

    [add]プロフィール編集ページを作成しました。

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,4 +2,7 @@ class AccountsController < ApplicationController
   def show
     @user = current_user
   end
+
+  def edit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   }
   get '/', to: 'tops#index'
   get 'about', to: 'tops#about'
-  resources :accounts, :only => [:show]
+  resources :accounts, :only => [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
