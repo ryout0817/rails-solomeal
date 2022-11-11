5652d744724ce76d0b86a5cef597e830b4fcb8c8
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-11 23:55:04 +0900

    [add]エラーメッセージにカラム名を追加しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
   mount_uploader :avatar, AvatarUploader
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
