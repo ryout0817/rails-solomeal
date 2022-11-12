cad93db418dda5c868e60b40399879c804521ce8
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-12 15:01:40 +0900

    [add]deviseのセッションコントローラーに新しいアクションを追加しました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+
+  devise_scope :user do {
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  }
   get '/', to: 'tops#index'
   get 'about', to: 'tops#about'
   resources :accounts, :only => [:show, :edit, :update]
