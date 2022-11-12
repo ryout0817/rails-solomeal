2ff71bf10f96eddf49aa858123438db09263c810
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-12 15:21:05 +0900

    [delete]tops_controller.rbのゲストログイン機能関連を削除しました。

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-  devise_scope :user do {
+  devise_scope :user do 
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
-  }
+  end
   get '/', to: 'tops#index'
   get 'about', to: 'tops#about'
   resources :accounts, :only => [:show, :edit, :update]
-  post '/tops/guest_sign_in', to: 'tops#guest_sign_in'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
