1ebfa7dd242fb51ea1328f201ac16a8bd1e12971
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-11-18 17:30:26 +0900

    [add]ユーザー退会機能の論理削除を追加しました。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -25,9 +25,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+      # 論理削除処理
+      soft_delete(current_user)
+      # Deviceの論理削除後の後処理
+      respond_with_navigational do
+        # 強制ログアウト
+        sign_out current_user
+        # ログアウト後のページ遷移
+        redirect_to "/"
+      end
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
@@ -59,4 +67,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  private
+
+  def soft_delete(user)
+    # 同じメールアドレスでも登録できるように、
+    # メールアドレスを“hoge@example.com_deleted_**********”に変更する
+    deleted_email = user.email + '_deleted_' + Time.current.to_i.to_s
+    user.assign_attributes(email: deleted_email, deleted_at: Time.current)
+    # 通常メールアドレスが変更されると通知メールが飛ぶので、
+    # その通知メールをキャンセルする
+    user.skip_email_changed_notification!
+    # 保存処理
+    user.save
+  end
 end
