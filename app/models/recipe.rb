eed868ef2a4c4122d7af9ca8b6d9e8be2ff81598
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-12-01 03:00:59 +0900

    [delete]レシピの紹介文のバリデーションを削除しました。

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,4 @@ class Recipe < ApplicationRecord
   mount_uploader :food, FoodUploader
 
   validates :recipe_name, presence: true, length: { in: 1..20 }
-  validates :recipe_introduction, presence: true, length: { in: 1..100 }
 end
