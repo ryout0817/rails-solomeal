ab01936b542f26446f6b862e4495feaaf919db50
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-12-01 12:57:37 +0900

    [add]レシピ投稿に必要なバリデーションを追加しました。

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,7 @@ class Recipe < ApplicationRecord
   mount_uploader :food, FoodUploader
 
   validates :recipe_name, presence: true, length: { in: 1..20 }
+  validates :price, presence: true
+  validates :people, presence: true
+  validates :main_food, presence: true
 end
