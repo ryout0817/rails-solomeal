d8be942498cc3971b833d4c09bd331097f8397dd
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-12-02 11:09:38 +0900

    [add]メールアドレスの変更時のみにパスワードが必要になるようにしました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, on: :configure_sign_up_params
 
   def self.guest
     user = User.find_or_create_by!(name: 'ゲスト', email: 'guest@example.com') do |user|
