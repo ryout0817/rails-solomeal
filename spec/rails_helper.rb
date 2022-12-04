35c08a86592defdfd9c3c15d842e9abce8404f9b
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-12-04 15:52:49 +0900

    [add]rspecにcurrent_userの機能を使用できるようにしました。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,6 +69,8 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include Warden::Test::Helpers
   config.extend ControllerMacros, :type => :controller
   config.include FactoryBot::Syntax::Methods
 end
