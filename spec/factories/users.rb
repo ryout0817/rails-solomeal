c2eb59817bffa722f1d2657f2016b93f7e23f77b
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-12-04 13:13:19 +0900

    [add]deviseでログインができるかをテストしました。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "test" }
+    sequence(:email) { |n| "TEST#{n}@example.com" }
+    password { "111111" }
+    password_confirmation {"111111"}
+    introduction {"よろしく"}
   end
 end
