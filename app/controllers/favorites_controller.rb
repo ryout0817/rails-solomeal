cb551c98712b4b2748be668fae949430ec023386
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-12-07 15:50:59 +0900

    [add]いいね機能の基本部分を追加しました。

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,20 @@
 class FavoritesController < ApplicationController
   def create
+    @user = current_user
+    @recipes = Recipe.all.page(params[:page]).order(created_at: :desc).per(5).includes(:user, :favorite)
+    favorite = Favorite.create(favorite_params)
+    redirect_to recipes_path
   end
 
   def destroy
+    if favorite = Favorite.find_by(favorite_params)
+      favorite.destroy
+    end
+    redirect_to recipes_path
+  end
+
+  private
+  def favorite_params
+    { recipe_id: params[:recipe_id], user_id: current_user.id }
   end
 end
