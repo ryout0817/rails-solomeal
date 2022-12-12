09d469e35147ddab4eda99a0faeb59da7b487cc9
Kami-Ryotaro <kamiryo.0817@gmail.com>
2022-12-13 06:53:11 +0900

    [add]トップページにランキング機能を追加しました。

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,6 +2,7 @@ class TopsController < ApplicationController
   def index
     @user = current_user
     @recipe = Recipe.all.order(created_at: :desc).first(1)
+    @recipe_rank = Recipe.find(Favorite.group(:recipe_id).order('count(recipe_id) desc').limit(3).pluck(:recipe_id))
   end
 
   def about
